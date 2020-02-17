5c1fd689633a549f0caf0d7928235d388df1a920
mihajlo96 <plecevicmihailo@gmail.com>
2020-02-17 17:48:35 +0100

    Ubaceni stimovi

--- Stimer.py
+++ Stimer.py
@@ -34,11 +34,10 @@ def open(fq):
     play_obj = sa.play_buffer(audio, 1, 2, fs)
 
 options = [
-    "lista1",
-    "lista2",
-    "Wednesday",
-    "Thursday",
-    "Friday"
+    "Standard",
+    "DropD",
+    "HalfStepDown",
+    "OpenG"
 ]
 
 zvuk1 = 82.41
@@ -47,8 +46,10 @@ zvuk3 = 146.8
 zvuk4 = 196
 zvuk5 = 246.9
 zvuk6 = 329.6
-lista1 = [82.41, 110, 146.8, 196, 246.9, 329.6]
-lista2 = [73.42, 110, 146.8, 196, 246.9, 329.6]
+Standard = [82.41, 110, 146.83, 196, 246.94, 329.63]
+DropD = [73.42, 110, 146.83, 196, 246.94, 329.63]
+HalfStepDown = [77.78, 103.83, 138.59, 185, 233.08, 311.13]
+OpenG = [73.42, 98, 146.83, 196, 246.94, 293.66]
 
 clicked = StringVar()
 clicked.set(options[0])
@@ -61,22 +62,60 @@ def zvuk(opcija):
     global zvuk5
     global zvuk6
 
-    if opcija == 'lista1':
-        zvuk1 = lista1[0]
-        zvuk2 = lista1[1]
-        zvuk3 = lista1[2]
-        zvuk4 = lista1[3]
-        zvuk5 = lista1[4]
-        zvuk6 = lista1[5]
+    if opcija == 'Standard':
+        dugme1.configure(text="E")
+        dugme2.configure(text="A")
+        dugme3.configure(text="D")
+        dugme4.configure(text="G")
+        dugme5.configure(text="H")
+        dugme6.configure(text="E")
+        zvuk1 = Standard[0]
+        zvuk2 = Standard[1]
+        zvuk3 = Standard[2]
+        zvuk4 = Standard[3]
+        zvuk5 = Standard[4]
+        zvuk6 = Standard[5]
 
-    elif opcija == 'lista2':
-        dugme1.configure(text="hello")
-        zvuk1 = lista2[0]
-        zvuk2 = lista2[1]
-        zvuk3 = lista2[2]
-        zvuk4 = lista2[3]
-        zvuk5 = lista2[4]
-        zvuk6 = lista2[5]
+    elif opcija == 'DropD':
+        dugme1.configure(text="D")
+        dugme2.configure(text="A")
+        dugme3.configure(text="D")
+        dugme4.configure(text="G")
+        dugme5.configure(text="H")
+        dugme6.configure(text="E")
+        zvuk1 = DropD[0]
+        zvuk2 = DropD[1]
+        zvuk3 = DropD[2]
+        zvuk4 = DropD[3]
+        zvuk5 = DropD[4]
+        zvuk6 = DropD[5]
+
+    elif opcija == 'HalfStepDown':
+        dugme1.configure(text="D#")
+        dugme2.configure(text="G#")
+        dugme3.configure(text="C#")
+        dugme4.configure(text="F#")
+        dugme5.configure(text="A#")
+        dugme6.configure(text="D#")
+        zvuk1 = HalfStepDown[0]
+        zvuk2 = HalfStepDown[1]
+        zvuk3 = HalfStepDown[2]
+        zvuk4 = HalfStepDown[3]
+        zvuk5 = HalfStepDown[4]
+        zvuk6 = HalfStepDown[5]
+    elif opcija == 'OpenG':
+        dugme1.configure(text="D")
+        dugme2.configure(text="G")
+        dugme3.configure(text="D")
+        dugme4.configure(text="G")
+        dugme5.configure(text="H")
+        dugme6.configure(text="D")
+        zvuk1 = OpenG[0]
+        zvuk2 = OpenG[1]
+        zvuk3 = OpenG[2]
+        zvuk4 = OpenG[3]
+        zvuk5 = OpenG[4]
+        zvuk6 = OpenG[5]
 
 drop = OptionMenu(root, clicked, *options, command=zvuk)
 drop.config(bg = "GREEN",fg= 'red')
@@ -88,22 +127,22 @@ drop.grid(row=0,column=3,padx=115)
 
 
 
-dugme1 = Button(root, text="420HZ",padx = 20, command=lambda: open(zvuk1) )
+dugme1 = Button(root, text="E",padx = 20, command=lambda: open(zvuk1) )
 dugme1.grid(row=1,column=3, pady = 20)
 
-dugme2 = Button(root, text="420HZ",padx = 20, command=lambda: open(zvuk2) )
+dugme2 = Button(root, text="A",padx = 20, command=lambda: open(zvuk2) )
 dugme2.grid(row=2,column=3)
 
-dugme3 = Button(root, text="420HZ",padx = 20, command=lambda: open(zvuk3) )
+dugme3 = Button(root, text="D",padx = 20, command=lambda: open(zvuk3) )
 dugme3.grid(row=3,column=3)
 
-dugme4 = Button(root, text="420HZ",padx = 20, command=lambda: open(zvuk4) )
+dugme4 = Button(root, text="G",padx = 20, command=lambda: open(zvuk4) )
 dugme4.grid(row=4,column=3)
 
-dugme5 = Button(root, text="420HZ",padx = 20, command=lambda: open(zvuk5) )
+dugme5 = Button(root, text="H",padx = 20, command=lambda: open(zvuk5) )
 dugme5.grid(row=5,column=3)
 
-dugme6 = Button(root, text="420HZ",padx = 20, command=lambda: open(zvuk6) )
+dugme6 = Button(root, text="E",padx = 20, command=lambda: open(zvuk6) )
 dugme6.grid(row=6,column=3)
 
 btn_stop = Button(root, text="STOP", command=lambda: stop() )
